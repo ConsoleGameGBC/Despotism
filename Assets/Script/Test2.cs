51b6c0313fb2b7a206016c29d24b291782c5a744
Merdemg <merdemgunay88@gmail.com>
2015-09-17 09:08:38 -0400

    Test
    Test! Test! TEST!

---
+++ Assets/Script/Test2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test2 : MonoBehaviour {
+	int testInt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
