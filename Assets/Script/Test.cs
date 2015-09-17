ad54c5b1002659292ba82f2527e7b808c521ad6d
Merdemg <merdemgunay88@gmail.com>
2015-09-17 08:58:09 -0400

    Test Stuff
    Is this thing on?

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	int testInt;
+	bool testBool;
+
+	// Use this for initialization
+	void Start () {
+		testInt = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
