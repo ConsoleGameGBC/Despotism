5a196c7a3ed23f6a08e1ad61d44ea857d9be94c4
vincentnihan <nihanyang@hotmail.com>
2015-10-07 12:14:21 +0800

    Finished File Folding + Made UI prefab + build up Event Camera + Set up script for future coding + correct mouse control for zoomIn/Out

--- Assets/Assist/UI/HighlightingSystemDemo/Scripts/CameraTargeting.cs
+++ Assets/Assist/UI/HighlightingSystemDemo/Scripts/CameraTargeting.cs
@@ -9,9 +9,12 @@ public class CameraTargeting : MonoBehaviour
 	private float startTime;
 	private float journeyLength;
 	private bool zoomIn = false;
+	private bool unfoldAlready = false;
 	// Which layers targeting ray must hit (-1 = everything)
 	public LayerMask targetingLayerMask = -1;
-	public Animator animator;
+	public GameObject leftPaper;
+	private Animator animatorZoom;
+	private Animator animatorFold;
 	// Targeting ray length
 	private float targetingRayLength = Mathf.Infinity;
 	
@@ -21,7 +24,8 @@ public class CameraTargeting : MonoBehaviour
 	void Start()
 	{
 		startTime = Time.time;
-		animator = GetComponent<Animator>();
+		animatorZoom = GetComponent<Animator>();
+		animatorFold = leftPaper.GetComponent<Animator>();
 	}
 
 	void Awake()
@@ -66,20 +70,18 @@ public class CameraTargeting : MonoBehaviour
 			HighlightableObject ho = targetTransform.root.GetComponentInChildren<HighlightableObject>();
 			if (ho != null)
 			{
-				if (Input.GetButtonDown("Fire1"))
+				if (Input.GetButtonDown("Fire1") && zoomIn != true)
 				{
-					if(zoomIn != true)
-					{
-						zoomIn = true;
-						animator.SetBool ("zoomIn MainMenu" ,zoomIn);
-					}
-					else
-					{
-						zoomIn = false;
-						animator.SetBool ("zoomIn MainMenu" ,zoomIn);
-					}
-
+					animatorZoom.SetBool ("zoomIn MainMenu" ,zoomIn = true);
+					animatorFold.SetBool ("pageUnfold" ,unfoldAlready = true);
 				}
+				else if(Input.GetButtonDown("Fire2") && zoomIn == true)
+				{
+					animatorZoom.SetBool ("zoomIn MainMenu" ,zoomIn = false);
+					animatorFold.SetBool ("pageUnfold" ,unfoldAlready = false);
+				}
+
+				if(zoomIn != true)
 				ho.On(Color.yellow);
 			}
 		}
