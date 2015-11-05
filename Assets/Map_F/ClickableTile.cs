b738e510a1b31fafa850c29e0fcbe148db95740f
vincentnihan <nihanyang@hotmail.com>
2015-11-05 12:05:47 -0500

    Chao's Map Update

--- Assets/Map_F/ClickableTile.cs
+++ Assets/Map_F/ClickableTile.cs
@@ -7,11 +7,50 @@ public class ClickableTile : MonoBehaviour {
 	public int tileY;
 	public TileMap map;
 
-	void OnMouseUp() {
-        Debug.Log(gameObject.tag + "  " + tileX + "," + tileY);
-       // Debug.Log(tileX + "," + tileY);
+    void OnMouseUp() {
+        Debug.Log(gameObject.tag);
+   //  Debug.Log(gameObject.tag + "  " + tileX + "," + tileY);
+    //   // Debug.Log(tileX + "," + tileY);
+        
+       //map.MoveSelectedUnitTo(tileX, tileY);
+    }
 
-		//map.MoveSelectedUnitTo(tileX, tileY);
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            tileY++;
+          
+         // Debug.Log(gameObject.tag + "  " + tileX + "," + tileY);
+            map.MoveSelectedUnitTo(tileX, tileY);
+          //  Debug.Log(tileX + "," + tileY);
+          
+        }
+
+        else  if (Input.GetKeyDown("s"))
+        {
+           tileY--;
+        
+        //    Debug.Log(gameObject.tag + "  " + tileX + "," + tileY);
+            map.MoveSelectedUnitTo(tileX, tileY);
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+            tileX--;
+       
+        //    Debug.Log(gameObject.tag + "  " + tileX + "," + tileY);
+            map.MoveSelectedUnitTo(tileX, tileY);
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            tileX++;
+           
+           // Debug.Log(gameObject.tag + "  " + tileX + "," + tileY);
+            map.MoveSelectedUnitTo(tileX, tileY);
+        }
+       
+    }
+
+
 
 }
--- Assets/Map_F/Unit.cs
+++ Assets/Map_F/Unit.cs
@@ -6,4 +6,24 @@ public class Unit : MonoBehaviour {
 	public int tileX;
 	public int tileY;
 
+
+
+    void Update()
+    {
+
+        Debug.Log(tileX + "," + tileY);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        //    Debug.Log(tileX + "," + tileY);
+        //  Debug.Log(other.gameObject.tag);
+
+        switch (other.transform.name)
+        {
+
+            case "Yellow": Debug.Log(other.gameObject.tag); break;
+        }
+    }
 }
