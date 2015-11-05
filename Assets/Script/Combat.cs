870ec92d1d74a9422900e8f1ccf9e2ef18e3b665
Merdemg <merdemgunay88@gmail.com>
2015-11-05 02:12:29 -0500

    Combat finished
    Sleep is overrated.

--- Assets/Script/Combat.cs
+++ Assets/Script/Combat.cs
@@ -6,32 +6,35 @@ public class Combat : MonoBehaviour {
     int soldiers;
     int enemyNum;
 
-    float skillFar = 0.3f;
-    float skillMed = 0.4f;
-    float skillMelee = 0.4f;
+    float playerRange = 0.35f;
+    float playerMelee = 0.35f;
 
+    float zombieRange = 0.0f;
+    float zombieMelee = 0.25f;
 
-    float zombieFar = 0.0f;
-    float zombieMed = 0.0f;
-    float zombieMelee = 0.4f;
+    float wolfRange = 0.0f;
+    float wolfMelee = 0.40f;
 
+    float rivalRange = 0.30f;
+    float rivalMelee = 0.30f;
 
-    float wolfFar = 0.0f;
-    float wolfMedium = 0.0f;
-    float wolfMelee = 0.7f;
+    float enemyRange;
+    float enemyMelee;
 
+    float cityRange = 0.1f;
+    float cityMelee = 1.0f;
 
+    float hillRange = 0.5f;
+    float hillMelee = 1.1f;
 
-    float grassFar = 1.1f;
-    float grassMedium = 1.1f;
-    float grassMelee = 1.0f;
+    float forestRange = 0.35f;
+    float forestMelee = 0.95f;
 
-    float cityFar = 0.1f;
-    float cityMedium = 0.5f;
-    float cityMelee = 1.2f;
+    float flatRange = 1.15f;
+    float flatMelee = 1.0f;
 
-    
-
+    float terrainRange;
+    float terrainMelee;
 
 	// Use this for initialization
 	void Start () {
@@ -45,7 +48,128 @@ public class Combat : MonoBehaviour {
 
     public string combatResult(int enemyNum, int enemyType, int terrainType, int soldierNum)
     {
-        return " ";
+
+        string myString = "We have encountered " + enemyNum.ToString() + " ";
+        switch (enemyType)
+        {
+            case 1:
+                enemyRange = zombieRange;
+                enemyMelee = zombieMelee;
+                myString += "zombies.";
+                break;
+            case 2:
+                enemyRange = wolfRange;
+                enemyMelee = wolfMelee;
+                myString += "wolves.";
+                break;
+            case 3:
+                enemyRange = rivalRange;
+                enemyMelee = rivalMelee;
+                myString += "gangers.";
+                break;
+        }
+        myString += " We fought ";
+        switch (terrainType)
+        {
+            case 1:
+                terrainRange = flatRange;
+                terrainMelee = flatMelee;
+                myString += "in flat wastelands.";
+                break;
+            case 2:
+                terrainRange = forestRange;
+                terrainMelee = forestMelee;
+                myString += "in a forest.";
+                break;
+            case 3:
+                terrainRange = cityRange;
+                terrainMelee = cityMelee;
+                myString += "in an urban area.";
+                break;
+            case 4:
+                terrainRange = hillRange;
+                terrainMelee = hillMelee;
+                myString += "on hills.";
+                break;
+        }
+
+        float enemyCas = soldierNum * terrainRange * playerRange * Random.Range(0.5f, 2.0f);
+        float playerCas = enemyNum * terrainRange * playerRange * Random.Range(0.5f, 2.0f);
+
+        if ((int)enemyCas > enemyNum)
+            enemyCas = enemyNum;
+
+        if ((int)playerCas > soldierNum)
+            playerCas = soldierNum;
+
+        soldierNum -= (int)playerCas;
+        enemyNum -= (int)enemyCas;
+
+        myString += " On range we killed " + ((int)enemyCas).ToString() + " and we lost "
+            + ((int)playerCas).ToString() + " soldiers";
+
+        if(soldierNum > 0 && enemyNum > 0)
+        {
+             enemyCas = soldierNum * terrainMelee * enemyMelee * Random.Range(0.5f, 2.0f);
+             playerCas = enemyNum * terrainMelee * playerMelee * Random.Range(0.5f, 2.0f);
+
+            if ((int)enemyCas > enemyNum)
+                enemyCas = enemyNum;
+
+            if ((int)playerCas > soldierNum)
+                playerCas = soldierNum;
+
+            soldierNum -= (int)playerCas;
+            enemyNum -= (int)enemyCas;
+
+
+            myString += " On melee we killed " + ((int)enemyCas).ToString() + " and we lost "
+            + ((int)playerCas).ToString() + " soldiers";
+        }
+
+        if (soldierNum > 0)
+        {
+            myString += " We won the fight.";
+        }
+        else
+        {
+            myString += "We lost the fight.";
+        }
+
+        soldiers = soldierNum;
+
+        return myString;
+    }
+
+    public string explorationResult(int enemyNum, int enemyType, int terrainType, int soldierNum)
+    {
+        string myString = combatResult(enemyNum, enemyType, terrainType, soldierNum);
+
+        if(soldiers > 0)
+        {
+            myString += "We looted: ";
+            int temp = Random.Range(0, 40);
+            myString += temp.ToString() + " food, ";
+            this.GetComponent<Resource>().changeFood(temp);
+
+            temp = Random.Range(0, 40);
+            myString += temp.ToString() + " water, ";
+            this.GetComponent<Resource>().changeWater(temp);
+
+            temp = Random.Range(0, 40);
+            myString += temp.ToString() + " fuel, ";
+            this.GetComponent<Resource>().changeFuel(temp);
+
+            temp = Random.Range(0, 40);
+            myString += temp.ToString() + " medicine, ";
+            this.GetComponent<Resource>().changeMedical(temp);
+
+            temp = Random.Range(0, 5);
+            myString += "We also found " + temp.ToString() + " survivors.";
+            this.GetComponent<Resource>().changeUnemployed(temp);
+        }
+
+        return myString;
     }
 
 
