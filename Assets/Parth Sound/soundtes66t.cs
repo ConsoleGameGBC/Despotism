d18aec0295ebf83e3f01d141e080815a1c17f8c1
parth13 <pkparthkakkad@gmail.com>
2015-11-19 15:18:12 -0500

    Parth Sound Folder Creted and Framework For sound Done

---
+++ Assets/Parth Sound/soundtes66t.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundtes66t : MonoBehaviour {
+	SoundManager sounds;
+	// Use this for initialization
+	void Start () {
+		 sounds = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<SoundManager>();
+
+		sounds.PlaySound (5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour 
+{
+	#region Game Object Singleton
+	
+	
+	public static SoundManager Instance = null;
+	public AudioClip[] Clips;
+	public  AudioSource dukeBox =null;
+	
+	
+	void Awake ()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			Object.DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			Object.Destroy(this.gameObject);
+		}
+	}
+	
+	
+	#endregion Game Object Singleton
+	
+
+	
+	public void  PlaySound(int clip)
+	{
+
+			
+			dukeBox.Stop();
+			dukeBox.clip=Clips[clip];
+			dukeBox.Play();
+
+
+	}
+}
