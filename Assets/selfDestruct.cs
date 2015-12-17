4c7c970e0b35c3c5fe73cad10595ee4c81453c7e
Merdemg <merdemgunay88@gmail.com>
2015-12-17 18:08:13 -0500

    control tips

---
+++ Assets/selfDestruct.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selfDestruct : MonoBehaviour {
+    [SerializeField] float lifeSpan = 3.0f;
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, lifeSpan);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
